bcae6eab09b609d10e65e0667c4af2049e3bd9f9
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2025-05-21 22:20:01 +0700

    Reaper code v2

---
+++ scripts/zombie/light.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name Light
+
+var on : bool
+
+func _ready() -> void:
+	on = false
+	_toggle_light()
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("toggle"):
+		on = !on
+		_toggle_light()
+
+func _toggle_light():
+	self.visible = true if on else false
+	$CollisionPolygon2D.set_deferred("disabled", false if on else true)
