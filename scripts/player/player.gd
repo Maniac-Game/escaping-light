9f26bf6dc97e1443ae171be25908f6e3adfc9504
Ahmad Ghozi Fidinillah <ahmadghozi1234@gmail.com>
2025-05-23 17:12:27 +0700

    add hp bar, and knockback effect when attacked by enemy

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,24 +3,33 @@ class_name Player
 
 @onready var animation_state_machine: StateMachine = $StateMachine/MovementState
 @export var health = 100
+@export var stamina = 100
+@onready var enemy: CharacterBody2D = null
+var current_state: State
 
 func _process(delta: float) -> void:
+	current_state = animation_state_machine.current_state
 	move_and_slide()
 	
 func launch_winscreen():
-	var current_state = animation_state_machine.current_state
 	animation_state_machine.on_child_transition(current_state,"disabled")
 	#winscreen.show()
 
 func get_state() -> State:
 	return animation_state_machine.current_state
 
+func get_health():
+	return health
+
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.name == "AttackArea":
+		enemy = area.get_parent()
 		print_debug("Health: ", health)
 		health -= 20
 		if health <= 0:
 			$Hitbox/CollisionShape2D.disabled = true
+			$BodyCollision.disabled = true
 			var current_state = animation_state_machine.current_state
 			animation_state_machine.on_child_transition(current_state,"death")
-			#trigger lose screen
+		else:
+			animation_state_machine.on_child_transition(current_state, "knockback")
